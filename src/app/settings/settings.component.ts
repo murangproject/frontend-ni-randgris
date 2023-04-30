05574af2e30ed3756abb65cd51df31a9b282c84a
redenv <redenv@careindeed.com>
2023-04-30 23:03:19 +0800

    completed settings

--- src/app/settings/settings.component.ts
+++ src/app/settings/settings.component.ts
@@ -1,24 +1,57 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SettingsTab } from './settings.enum';
+import { Room, Section, SettingsService, Subject } from './settings.service';
+import { FormsModule, ReactiveFormsModule, UntypedFormBuilder, Validators } from '@angular/forms';
+import { map } from 'rxjs';
 
 @Component({
   selector: 'app-settings',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, FormsModule, ReactiveFormsModule],
   templateUrl: './settings.component.html',
   styles: [
   ]
 })
-export class SettingsComponent {
+export class SettingsComponent implements OnInit {
+  sections$ = this.settings.getSections().pipe(
+    map(sections => sections.filter(section => !section.is_deleted))
+  );
+  subjects$ = this.settings.getSubjects().pipe(
+    map(subjects => subjects.filter(subject => !subject.is_deleted))
+  );
+  rooms$ = this.settings.getRooms().pipe(
+    map(rooms => rooms.filter(room => !room.is_deleted))
+  );
+
+  sectionForm = this.formBuilder.group({
+    name: ['', [Validators.required]]
+  });
+
+  subjectForm = this.formBuilder.group({
+    code: ['', [Validators.required]],
+    name: ['', [Validators.required]]
+  });
+
+  roomForm = this.formBuilder.group({
+    room_name: ['', [Validators.required]],
+  });
+
+  constructor(private settings: SettingsService, private formBuilder: UntypedFormBuilder) { }
+
+  ngOnInit(): void {
+    this.settings.initSections();
+    this.settings.initSubjects();
+    this.settings.initRooms();
+  }
+
+  selectedSectionId: number = -1;
+  selectedSubjectId: number = -1;
+  selectedRoomId: number = -1;
 
   currentTab: SettingsTab = SettingsTab.SECTION;
 
   // Modal State
-  createSemesterModalState: boolean = false;
-  editSemesterModalState: boolean = false;
-  archiveSemesterModalState: boolean = false;
-
   createSectionModalState: boolean = false;
   editSectionModalState: boolean = false;
   archiveSectionModalState: boolean = false;
@@ -33,33 +66,6 @@ export class SettingsComponent {
 
   // Modal Functions
 
-  // Create Semester
-  openCreateSemesterModal() {
-    this.createSemesterModalState = true;
-  }
-
-  closeCreateSemesterModal() {
-    this.createSemesterModalState = false;
-  }
-
-  // Edit Semester
-  openEditSemesterModal() {
-    this.editSemesterModalState = true;
-  }
-
-  closeEditSemesterModal() {
-    this.editSemesterModalState = false;
-  }
-
-  // Archive Semester
-  openArchiveSemesterModal() {
-    this.archiveSemesterModalState = true;
-  }
-
-  closeArchiveSemesterModal() {
-    this.archiveSemesterModalState = false;
-  }
-
   // Create Section
   openCreateSectionModal() {
     this.createSectionModalState = true;
@@ -67,24 +73,55 @@ export class SettingsComponent {
 
   closeCreateSectionModal() {
     this.createSectionModalState = false;
+    this.sectionForm.reset();
+  }
+
+  onSubmitCreateSection() {
+    this.settings.createSection(this.sectionForm.value).subscribe(data => {
+      this.settings.initSections();
+      this.closeCreateSectionModal();
+    });
   }
 
   // Edit Section
-  openEditSectionModal() {
+  openEditSectionModal(id: number) {
+    this.selectedSectionId = id;
     this.editSectionModalState = true;
+    this.sections$.subscribe((sections: Section[]) => {
+      this.sectionForm.patchValue(sections.find((section: Section) => section?.id === id) ?? {});
+      console.log(this.sectionForm.value);
+    });
   }
 
   closeEditSectionModal() {
+    this.selectedSectionId = -1;
     this.editSectionModalState = false;
+    this.sectionForm.reset();
+  }
+
+  onSubmitEditSection() {
+    this.settings.updateSection(this.selectedSectionId, this.sectionForm.value).subscribe(data => {
+      this.settings.initSections();
+      this.closeEditSectionModal();
+    });
   }
 
   // Archive Section
-  openArchiveSectionModal() {
+  openArchiveSectionModal(id: number) {
+    this.selectedSectionId = id;
     this.archiveSectionModalState = true;
   }
 
   closeArchiveSectionModal() {
+    this.selectedSectionId = -1;
     this.archiveSectionModalState = false;
+  }
+
+  onSubmitArchiveSection() {
+    this.settings.deleteSection(this.selectedSectionId).subscribe(data => {
+      this.closeArchiveSectionModal();
+      this.settings.initSections();
+    });
   }
 
   // Create Subject
@@ -94,24 +131,55 @@ export class SettingsComponent {
 
   closeCreateSubjectModal() {
     this.createSubjectModalState = false;
+    this.subjectForm.reset();
+  }
+
+  onSubmitCreateSubject() {
+    this.settings.createSubject(this.subjectForm.value).subscribe(data => {
+      this.settings.initSubjects();
+      this.closeCreateSubjectModal();
+    });
   }
 
   // Edit Subject
-  openEditSubjectModal() {
+  openEditSubjectModal(id: number) {
+    this.selectedSubjectId = id;
     this.editSubjectModalState = true;
+    this.subjects$.subscribe((subjects: Subject[]) => {
+      this.subjectForm.patchValue(subjects.find((subject: Subject) => subject?.id === id) ?? {});
+      console.log(this.subjectForm.value);
+    });
   }
 
   closeEditSubjectModal() {
+    this.selectedSubjectId = -1;
     this.editSubjectModalState = false;
+    this.subjectForm.reset();
+  }
+
+  onSubmitEditSubject() {
+    this.settings.updateSubject(this.selectedSubjectId, this.subjectForm.value).subscribe(data => {
+      this.settings.initSubjects();
+      this.closeEditSubjectModal();
+    });
   }
 
   // Archive Subject
-  openArchiveSubjectModal() {
+  openArchiveSubjectModal(id: number) {
+    this.selectedSubjectId = id;
     this.archiveSubjectModalState = true;
   }
 
   closeArchiveSubjectModal() {
+    this.selectedSubjectId = -1;
     this.archiveSubjectModalState = false;
+  }
+
+  onSubmitArchiveSubject() {
+    this.settings.deleteSubject(this.selectedSubjectId).subscribe(data => {
+      this.closeArchiveSubjectModal();
+      this.settings.initSubjects();
+    });
   }
 
   // Create Room
@@ -121,24 +189,55 @@ export class SettingsComponent {
 
   closeCreateRoomModal() {
     this.createRoomModalState = false;
+    this.roomForm.reset();
+  }
+
+  onSubmitCreateRoom() {
+    this.settings.createRoom(this.roomForm.value).subscribe(data => {
+      this.settings.initRooms();
+      this.closeCreateRoomModal();
+    });
   }
 
   // Edit Room
-  openEditRoomModal() {
+  openEditRoomModal(id: number) {
+    this.selectedRoomId = id;
     this.editRoomModalState = true;
+    this.rooms$.subscribe((rooms: Room[]) => {
+      this.roomForm.patchValue(rooms.find((room: Room) => room?.id === id) ?? {});
+      console.log(this.roomForm.value);
+    });
   }
 
   closeEditRoomModal() {
+    this.selectedRoomId = -1;
     this.editRoomModalState = false;
+    this.roomForm.reset();
+  }
+
+  onSubmitEditRoom() {
+    this.settings.updateRoom(this.selectedRoomId, this.roomForm.value).subscribe(data => {
+      this.settings.initRooms();
+      this.closeEditRoomModal();
+    });
   }
 
   // Archive Room
-  openArchiveRoomModal() {
+  openArchiveRoomModal(id: number) {
+    this.selectedRoomId = id;
     this.archiveRoomModalState = true;
   }
 
   closeArchiveRoomModal() {
+    this.selectedRoomId = -1;
     this.archiveRoomModalState = false;
+  }
+
+  onSubmitArchiveRoom() {
+    this.settings.deleteRoom(this.selectedRoomId).subscribe(data => {
+      this.closeArchiveRoomModal();
+      this.settings.initRooms();
+    });
   }
 
   setTab(tab: string) {
---
+++ src/app/settings/settings.service.ts
@@ -0,0 +1,227 @@
+import { Injectable } from "@angular/core";
+import { HttpClient } from "@angular/common/http";
+import { BehaviorSubject, shareReplay } from "rxjs";
+import { environment } from "src/environments/environment";
+
+export interface Section {
+  id?: number;
+  name?: string;
+  is_deleted?: boolean;
+}
+
+export interface CreateSectionDto {
+  name: string;
+}
+
+export interface UpdateSectionDto extends Partial<CreateSectionDto> {
+  is_deleted?: boolean;
+}
+
+export interface Subject {
+  id?: number;
+  code?: string;
+  name?: string;
+  is_deleted?: boolean;
+}
+
+export interface CreateSubjectDto {
+  code: string;
+  name: string;
+}
+
+export interface UpdateSubjectDto extends Partial<CreateSubjectDto> {
+  is_deleted?: boolean;
+}
+
+export interface Room {
+  id?: number;
+  room_name?: string;
+  borrow_time?: string;
+  return_time?: string;
+  status?: 'available' | 'occupied';
+  is_deleted?: boolean;
+}
+
+export interface CreateRoomDto {
+  room_name: string;
+}
+
+export interface UpdateRoomDto extends Partial<CreateRoomDto> {
+  borrow_time?: string;
+  return_time?: string;
+  status?: 'available' | 'occupied';
+  is_deleted?: boolean;
+}
+
+
+@Injectable({
+  providedIn: "root"
+})
+export class SettingsService {
+  sections$ = new BehaviorSubject<Section[]>([]);
+  subjects$ = new BehaviorSubject<Subject[]>([]);
+  rooms$ = new BehaviorSubject<Room[]>([]);
+
+  constructor(private http: HttpClient) { }
+
+  initSections() {
+    const headers = {
+      'Content-Type': 'application/json',
+      'Accept': 'application/json',
+      'Authorization': `Bearer ${localStorage.getItem('token')}`
+    }
+    this.http.get<Section[]>(`${environment.apiUrl}/sections`, { headers: headers }).subscribe(
+      (res: Section[]) => this.sections$.next(res)
+    );
+  }
+
+  getSections() {
+    return this.sections$.asObservable().pipe(
+      shareReplay({ bufferSize: 1, refCount: true })
+    );
+  }
+
+  initSubjects() {
+    const headers = {
+      'Content-Type': 'application/json',
+      'Accept': 'application/json',
+      'Authorization': `Bearer ${localStorage.getItem('token')}`
+    }
+
+    this.http.get<Subject[]>(`${environment.apiUrl}/subjects`, { headers: headers }).subscribe(
+      (res: Subject[]) => this.subjects$.next(res)
+    );
+  }
+
+  getSubjects() {
+    return this.subjects$.asObservable().pipe(
+      shareReplay({ bufferSize: 1, refCount: true })
+    );
+  }
+
+  initRooms() {
+    const headers = {
+      'Content-Type': 'application/json',
+      'Accept': 'application/json',
+      'Authorization': `Bearer ${localStorage.getItem('token')}`
+    }
+
+    this.http.get<Room[]>(`${environment.apiUrl}/rooms`, { headers: headers }).subscribe(
+      (res: Room[]) => this.rooms$.next(res)
+    );
+  }
+
+  getRooms() {
+    return this.rooms$.asObservable().pipe(
+      shareReplay({ bufferSize: 1, refCount: true })
+    );
+  }
+
+  createSection(section: CreateSectionDto) {
+    const headers = {
+      'Content-Type': 'application/json',
+      'Accept': 'application/json',
+      'Authorization': `Bearer ${localStorage.getItem('token')}`
+    }
+
+    return this.http.post(`${environment.apiUrl}/sections`, section, { headers: headers }).pipe(
+      shareReplay({ bufferSize: 1, refCount: true })
+    );
+  }
+
+  updateSection(id: number, section: UpdateSectionDto) {
+    const headers = {
+      'Content-Type': 'application/json',
+      'Accept': 'application/json',
+      'Authorization': `Bearer ${localStorage.getItem('token')}`
+    }
+
+    return this.http.put(`${environment.apiUrl}/sections/${id}`, section, { headers: headers }).pipe(
+      shareReplay({ bufferSize: 1, refCount: true })
+    );
+  }
+
+  deleteSection(id: number) {
+    const headers = {
+      'Content-Type': 'application/json',
+      'Accept': 'application/json',
+      'Authorization': `Bearer ${localStorage.getItem('token')}`
+    }
+
+    return this.http.delete(`${environment.apiUrl}/sections/${id}`, { headers: headers }).pipe(
+      shareReplay({ bufferSize: 1, refCount: true })
+    );
+  }
+
+  createSubject(subject: CreateSubjectDto) {
+    const headers = {
+      'Content-Type': 'application/json',
+      'Accept': 'application/json',
+      'Authorization': `Bearer ${localStorage.getItem('token')}`
+    }
+
+    return this.http.post(`${environment.apiUrl}/subjects`, subject, { headers: headers }).pipe(
+      shareReplay({ bufferSize: 1, refCount: true })
+    );
+  }
+
+  updateSubject(id: number, subject: UpdateSubjectDto) {
+    const headers = {
+      'Content-Type': 'application/json',
+      'Accept': 'application/json',
+      'Authorization': `Bearer ${localStorage.getItem('token')}`
+    }
+
+    return this.http.put(`${environment.apiUrl}/subjects/${id}`, subject, { headers: headers }).pipe(
+      shareReplay({ bufferSize: 1, refCount: true })
+    );
+  }
+
+  deleteSubject(id: number) {
+    const headers = {
+      'Content-Type': 'application/json',
+      'Accept': 'application/json',
+      'Authorization': `Bearer ${localStorage.getItem('token')}`
+    }
+
+    return this.http.delete(`${environment.apiUrl}/subjects/${id}`, { headers: headers }).pipe(
+      shareReplay({ bufferSize: 1, refCount: true })
+    );
+  }
+
+  createRoom(room: CreateRoomDto) {
+    const headers = {
+      'Content-Type': 'application/json',
+      'Accept': 'application/json',
+      'Authorization': `Bearer ${localStorage.getItem('token')}`
+    }
+
+    return this.http.post(`${environment.apiUrl}/rooms`, room, { headers: headers }).pipe(
+      shareReplay({ bufferSize: 1, refCount: true })
+    );
+  }
+
+  updateRoom(id: number, room: UpdateRoomDto) {
+    const headers = {
+      'Content-Type': 'application/json',
+      'Accept': 'application/json',
+      'Authorization': `Bearer ${localStorage.getItem('token')}`
+    }
+
+    return this.http.put(`${environment.apiUrl}/rooms/${id}`, room, { headers: headers }).pipe(
+      shareReplay({ bufferSize: 1, refCount: true })
+    );
+  }
+
+  deleteRoom(id: number) {
+    const headers = {
+      'Content-Type': 'application/json',
+      'Accept': 'application/json',
+      'Authorization': `Bearer ${localStorage.getItem('token')}`
+    }
+
+    return this.http.delete(`${environment.apiUrl}/rooms/${id}`, { headers: headers }).pipe(
+      shareReplay({ bufferSize: 1, refCount: true })
+    );
+  }
+}
