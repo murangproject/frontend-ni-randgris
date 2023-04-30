81d8d5a0d6f286c1741cd545ec01f999b28e8974
redenv <redenv@careindeed.com>
2023-05-01 06:15:37 +0800

    added github workflows

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -16,14 +16,17 @@ const routes: Routes = [
     children: [
       {
         path: 'login',
+        title: 'Login Page',
         loadComponent: () => import('./login/login.component').then(c => c.LoginComponent)
       },
       {
         path: 'logout',
+        title: 'Logout Page',
         loadComponent: () => import('./logout/logout.component').then(c => c.LogoutComponent)
       },
       {
         path: 'init',
+        title: 'Account Initialize',
         loadComponent: () => import('./init/init.component').then(c => c.InitComponent)
       },
       {
@@ -33,60 +36,70 @@ const routes: Routes = [
         children: [
           {
             path: 'dashboard',
+            title: 'Dashboard',
             data: { roles: ['admin', 'attendance_checker'] },
             canMatch: [roleGuard],
             loadComponent: () => import('./dashboard/dashboard.component').then(c => c.DashboardComponent)
           },
           {
             path: 'users',
+            title: 'Users Management',
             data: { roles: ['admin'] },
             canMatch: [roleGuard],
             loadComponent: () => import('./users/users.component').then(c => c.UsersComponent)
           },
           {
             path: 'faculties',
+            title: 'Faculties Management',
             data: { roles: ['admin', 'attendance_checker'] },
             canMatch: [roleGuard],
             loadComponent: () => import('./faculty/faculty.component').then(c => c.FacultyComponent)
           },
           {
             path: 'faculties/:id',
+            title: 'Faculty',
             data: { roles: ['admin', 'attendance_checker'] },
             canMatch: [roleGuard],
             loadComponent: () => import('./faculty-schedules/faculty-schedules.component').then(c => c.FacultySchedulesComponent)
           },
           {
             path: 'faculties/:id/attendance',
+            title: 'Faculty Attendance',
             data: { roles: ['admin', 'attendance_checker', 'faculty'] },
             canMatch: [roleGuard],
             loadComponent: () => import('./faculty-attendance/faculty-attendance.component').then(c => c.FacultyAttendanceComponent)
           },
           {
             path: 'attendances',
+            title: 'Attendances',
             data: { roles: ['admin', 'attendance_checker', 'faculty'] },
             canMatch: [roleGuard],
             loadComponent: () => import('./attendances/attendances.component').then(c => c.AttendancesComponent)
           },
           {
             path: 'schedules',
+            title: 'Schedules',
             data: { roles: ['faculty'] },
             canMatch: [roleGuard],
             loadComponent: () => import('./schedules/schedules.component').then(c => c.SchedulesComponent)
           },
           {
             path: 'attendance-records',
+            title: 'Attendance Records',
             data: { roles: ['faculty'] },
             canMatch: [roleGuard],
             loadComponent: () => import('./profile/profile.component').then(c => c.ProfileComponent)
           },
           {
             path: 'keys',
+            title: 'Keys',
             data: { roles: ['admin', 'attendance_checker', 'faculty'] },
             canMatch: [roleGuard],
             loadComponent: () => import('./keys/keys.component').then(c => c.KeysComponent)
           },
           {
             path: 'settings',
+            title: 'Settings',
             loadComponent: () => import('./settings/settings.component').then(c => c.SettingsComponent)
           }
         ],
--- src/app/sidebar/sidebar.component.ts
+++ src/app/sidebar/sidebar.component.ts
@@ -1,21 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { Navbar } from './menu-list';
-import { RouterModule } from '@angular/router';
+import { ActivatedRoute, Router, RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-sidebar',
   standalone: true,
   imports: [CommonModule, RouterModule],
-  templateUrl: './sidebar.component.html',
-  styles: [
-  ]
+  templateUrl: './sidebar.component.html'
 })
-export class SidebarComponent {
+export class SidebarComponent implements OnInit {
+  title = '';
   navBarItems: any;
 
-  constructor() {
+  constructor(private activatedRoute: ActivatedRoute, private router: Router) {
     const role = localStorage.getItem('role');
     this.navBarItems = Navbar[role as keyof typeof Navbar];
+  }
+  ngOnInit(): void {
+    this.router.events.subscribe(() => {
+      this.activatedRoute.firstChild?.title.subscribe(data => {
+        this.title = data ?? '';
+      });
+    });
   }
 }
